a7a72de73d96787b499f5ed62e0af6145f675bfa
i-soroka <soroka.igor.olegovych@gmail.com>
2024-05-20 09:54:16 +0300

    init

---
+++ src/main/java/io/parser/msoffice/MsOfficeParserApplication.java
@@ -0,0 +1,13 @@
+package io.parser.msoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOfficeParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOfficeParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/parser/msoffice/MsOfficeParserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.parser.msoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOfficeParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
